5a73bf947dc4553fde5234368274df3c2ce040fa
additi123 <mehtaadditi@gmail.com>
2026-01-30 22:11:00 -0500

    execution of commands

--- git/repo.go
+++ git/repo.go
@@ -6,4 +6,29 @@ import (
 	"strings"
 )
 
-func runGit(args ...string) (string, error)
+func runGit(args ...string) (string, error) {
+	cmd := exec.Command("git", args...)
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	cmd.Stderr = &out
+	err := cmd.Run()
+	return strings.TrimSpace(out.String()), err
+}
+
+
+func CurrentBranch() (string, error) {
+	return runGit("branch", "--show-current")
+}
+
+func MergeBase(base string) (string, error) {
+	return runGit("merge-base", base, "HEAD")
+}
+
+func AheadBehind(base string) (ahead, behind string, err error) {
+	out, err := runGit("rev-list", "--left-right", "--count", base+"...HEAD")
+	if err != nil {
+		return "", "", err
+	}
+	parts := strings.Split(out, "\t")
+	return parts[1], parts[0], nil
+}
